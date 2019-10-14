ddd12e6ba80992b0e8b1972b954f990ce288193c
MitraDarja <mdarvish@posteo.de>
2019-10-14 12:03:58 +0200

    Update and rename count_minimizer_occurences3.cpp to count.cpp

--- count.cpp
+++ count.cpp
@@ -5,8 +5,8 @@
 
 #include <seqan3/alphabet/nucleotide/dna4.hpp>
 #include <seqan3/argument_parser/all.hpp>
+#include <seqan3/io/stream/debug_stream.hpp>
 #include <seqan3/io/sequence_file/all.hpp>
-//#include <seqan3/io/stream/debug_stream.hpp>
 #include <seqan3/std/filesystem>
 #include <seqan3/std/ranges>
 
@@ -31,7 +31,7 @@ struct my_traits : sequence_file_input_default_traits_dna
 struct cmd_arguments
 {
     std::filesystem::path file_path_fasta{};
-    std::filesystem::path file_path_exon;
+    std::filesystem::path file_path_gene;
     std::filesystem::path file_path_out{"out.txt"};
     uint8_t k{20};
     uint16_t window_size{60};
@@ -44,7 +44,7 @@ void initialize_argument_parser(argument_parser & parser, cmd_arguments & args)
     parser.info.author = "Mitra Darvish";
     parser.info.short_description = "Calculates Minimizer.";
     parser.add_positional_option(args.file_path_fasta, "Please provide a fasta file.");
-    parser.add_option(args.file_path_exon, 'e', "exon", "Fasta file with exon information.");
+    parser.add_option(args.file_path_gene, 'g', "gene", "Fasta file with gene information.");
     parser.add_option(args.file_path_out, 'o', "out", "File to which the output is saved to.");
     parser.add_option(args.k, 'k', "kmer", "Define kmer size.");
     parser.add_option(args.window_size, 'w', "window", "Define window size.");
@@ -52,36 +52,6 @@ void initialize_argument_parser(argument_parser & parser, cmd_arguments & args)
                       "position to be ignored, 1 a position considered. Default: ungapped.");
     parser.add_option(args.seed, 's', "seed", "Define seed.");
 }
-
-/*
-std::vector<uint64_t> compute_minimizer(dna4_vector & seq)
-{
-//    size_t const window_size = ((length(seq) - k) / LeastNumMinimizers) + k; // ensure at least 3 minimizer
-    if (k > window_size)
-        throw std::logic_error("Cannot divide the sequence into the specified window size."); // return std::array<uint64_t, 3> {};
-
-    Minimizer minimizer;
-    minimizer.resize(k, window_size, seed);
-
-    return minimizer.getMinimizer(seq);
-}
-
-
-auto compute_occurrences(std::vector<dna4_vector> & seqs)
-{
-    std::unordered_map<uint64_t, uint32_t> occurring_kmers{};
-
-    for (auto & seq : seqs)
-    {
-        for (auto & minHash : compute_minimizer(seq))
-        {
-            ++occurring_kmers[minHash];
-        }
-    }
-
-    return occurring_kmers;
-}
-*/
 
 int main(int const argc, char const ** argv)
 {
@@ -96,7 +66,6 @@ int main(int const argc, char const ** argv)
     }
     catch (parser_invalid_argument const & ext)                     // catch user errors
     {
-        //debug_stream << "Error. Incorrect command line input for minimizer." << ext.what() << "\n";
         return -1;
     }
 
@@ -123,7 +92,6 @@ int main(int const argc, char const ** argv)
     auto end = std::chrono::steady_clock::now();
     std::cerr << "Time: " << std::chrono::duration_cast<std::chrono::seconds>(end-start).count() << "s" << std::endl << std::endl;
 
-    //window_size = ((length(seqs[0]) - k) / min_num_minimizer_per_read) + k; // ensure at least 3 minimizer
     std::cerr << "Window size used for this set: " << args.window_size << std::endl;
 
     std::cerr << "Computing minimizers in all sequences." << std::endl;
@@ -141,44 +109,36 @@ int main(int const argc, char const ** argv)
         for (auto & minHash : compute_minimizer(seq, k, window_size, args.shape, seed))
         {
             counts.push_back(hash_table[minHash]);
-            outfile << ids[seq_idx] << sep << minHash << "\n";
         }
         std::nth_element(counts.begin(), counts.begin() + counts.size()/2, counts.end());
-        if ( 1 < counts[counts.size()/2] )
-            debug_stream << ids[seq_idx] << sep << counts[counts.size()/2] << "\n";
         counts.clear();
         seq_idx++;
     }
 
-    if(args.file_path_exon != "") // exon file given
+    if(args.file_path_gene != "") // gene file given
     {
-        // assume a exon fasta file was given
-        std::vector<std::string> exon_ids;
-        std::vector<dna4_vector> exon_seqs;
+        // assume a gene fasta file was given
+        std::vector<std::string> gene_ids;
+        std::vector<dna4_vector> gene_seqs;
 
-        sequence_file_input<my_traits> gene_file{args.file_path_exon};
+        sequence_file_input<my_traits> gene_file{args.file_path_gene};
         for (auto & rec : gene_file)
         {
-          exon_ids.push_back(get<field::ID>(rec));
-          exon_seqs.push_back(get<field::SEQ>(rec));
+          gene_ids.push_back(get<field::ID>(rec));
+          gene_seqs.push_back(get<field::SEQ>(rec));
         }
 
-        for (size_t idx = 0; idx < exon_ids.size(); ++idx)
+        for (size_t idx = 0; idx < gene_ids.size(); ++idx)
         {
-            //outfile << exon_ids[idx] << "\t" << idx << "\t[";
-            //debug_stream << exon_ids[idx] << "\n";
-            for (auto hash : compute_minimizer(exon_seqs[idx], k, window_size, args.shape, seed))
+            outfile << gene_ids[idx] << "\t" << idx << "\t[";
+            for (auto hash : compute_minimizer(gene_seqs[idx], k, window_size, args.shape, seed))
             {
-            //    outfile << hash_table[hash] << ",";
+                outfile << hash_table[hash] << ",";
                 counts.push_back(hash_table[hash]);
-            //    debug_stream << hash << " ";
             }
-            //outfile << "]\n";
+            outfile << "]\n";
             std::nth_element(counts.begin(), counts.begin() + counts.size()/2, counts.end());
-            //std::sort(counts.begin(), counts.end());
-            //debug_stream << counts << "\n";
-            //if (counts[counts.size()/2] > 1)
-            debug_stream << exon_ids[idx] << sep << counts[counts.size()/2] << sep << idx << "\n";
+            debug_stream << gene_ids[idx] << sep << counts[counts.size()/2] << sep << counts << "\n";
 
             counts.clear();
         }
