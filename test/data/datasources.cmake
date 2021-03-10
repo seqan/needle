4d226dace35968747729f8de035e2dc11a694663
Mitra Darja Darvish <mitra.darvish@fu-berlin.de>
2021-03-10 17:05:39 +0100

    [FIX] Datasource

--- test/data/datasources.cmake
+++ test/data/datasources.cmake
@@ -8,7 +8,7 @@ declare_datasource (FILE exp_01.fasta
                     URL_HASH SHA256=e7236e7b86303d84a86a4454044125005433857416183cdaac0f4cdf7ac34e06)
 declare_datasource (FILE IBF_1
                     URL ${CMAKE_SOURCE_DIR}/test/data/IBF_1
-                    URL_HASH SHA256=e752c7d281ffa8a5104c6c6bdfd0c4868d39cf98bb26e95c4adbfe52c9eb6784)
+                    URL_HASH SHA256=c41b34c5853e2ca738d89171f2ca04662d99e7f0d7498b0f5c555cdc9af7d814)
 declare_datasource (FILE mini_example.fasta
                     URL ${CMAKE_SOURCE_DIR}/test/data/mini_example.fasta
                     URL_HASH SHA256=f872221632e7423b071d1aedaf4c4e9da2a659e843fcafac12cd65632b904b93)
