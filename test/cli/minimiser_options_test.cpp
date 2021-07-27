3a41d3f4d2dda0553090f128ae0ca556b5d1b1c4
Mitra Darja Darvish <mitra.darvish@fu-berlin.de>
2021-07-27 18:32:34 +0200

    [TEST] Test more options for minimiser.

--- test/cli/minimiser_options_test.cpp
+++ test/cli/minimiser_options_test.cpp
@@ -29,9 +29,44 @@ TEST_F(cli_test, fail_no_argument)
     EXPECT_EQ(result.err, expected);
 }
 
-TEST_F(cli_test, with_argument)
+TEST_F(cli_test, with_arguments)
 {
-    cli_test_result result = execute_app("needle minimiser -k 4 -w 8", data("mini_example.fasta"));
+    cli_test_result result = execute_app("needle minimiser -k 4 -w 4 -s 0", data("mini_example.fasta"));
+    EXPECT_EQ(result.exit_code, 0);
+    EXPECT_EQ(result.out, "");
+    EXPECT_EQ(result.err, std::string{});
+}
+
+TEST_F(cli_test, cutoff)
+{
+    cli_test_result result = execute_app("needle minimiser -k 4 -w 8 -s 0 -u 2", data("mini_example.fasta"));
+    EXPECT_EQ(result.exit_code, 0);
+    EXPECT_EQ(result.out, "");
+    EXPECT_EQ(result.err, std::string{});
+}
+
+TEST_F(cli_test, multiple_sample)
+{
+    cli_test_result result = execute_app("needle minimiser -k 4 -w 8 -s 0 -m 2 ", data("mini_example.fasta"),
+                                                                                  data("mini_example.fasta"));
+    EXPECT_EQ(result.exit_code, 0);
+    EXPECT_EQ(result.out, "");
+    EXPECT_EQ(result.err, std::string{});
+}
+
+TEST_F(cli_test, multithreads)
+{
+    cli_test_result result = execute_app("needle minimiser -k 4 -w 8 -s 0 -t 2", data("mini_example.fasta"),
+                                                                                 data("mini_example.fasta"));
+    EXPECT_EQ(result.exit_code, 0);
+    EXPECT_EQ(result.out, "");
+    EXPECT_EQ(result.err, std::string{});
+}
+
+TEST_F(cli_test, paired)
+{
+    cli_test_result result = execute_app("needle minimiser -k 4 -w 8 -s 0 -q ", data("mini_example.fasta"),
+                                                                                data("mini_example.fasta"));
     EXPECT_EQ(result.exit_code, 0);
     EXPECT_EQ(result.out, "");
     EXPECT_EQ(result.err, std::string{});
