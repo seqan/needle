5b6b86e1b2f9b15fa2e2d35c5804793079729f8e
mitradarja <mitra.darvish@fu-berlin.de>
2022-05-30 19:14:49 +0200

    [FEATURE] Add using multiple threads as option to estimate.

--- src/main.cpp
+++ src/main.cpp
@@ -136,6 +136,7 @@ int run_needle_estimate(seqan3::argument_parser & parser)
     parser.add_positional_option(estimate_args.search_file, "Please provide a sequence file.");
     parser.add_option(estimate_args.path_in, 'i', "in", "Directory where input files can be found.");
     parser.add_option(args.path_out, 'o', "out", "Directory, where output files should be saved.");
+    parser.add_option(args.threads, 't', "threads", "Number of threads to use. Default: 1.");
     parser.add_flag(estimate_args.normalization_method, 'm', "normalization-mode",
                                                             "Set, if normalization is wanted. Normalization is achieved by"
                                                             "dividing the expression value with the expression threshold of the first"
