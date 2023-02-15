89dae9b62d0d14100698dd06ceb01de0fcdec7ee
Mitra Darja Darvish <mdarvish@posteo.de>
2023-02-15 13:52:59 +0100

    [FEATURE] Add genome as preprocess step to count.

--- test/data/datasources.cmake
+++ test/data/datasources.cmake
@@ -23,7 +23,7 @@ declare_datasource (FILE mini_gen2.fasta
                     URL_HASH SHA256=7e21b3eff20f950e6a0fa2369a0bbbed4cc967a24083876350f839f4a232771b)
 declare_datasource (FILE mini_gen.genome
                     URL ${CMAKE_SOURCE_DIR}/test/data/mini_gen.genome
-                    URL_HASH SHA256=6e9da2f6693938586c902f5e4445f2df1f1ac94cff8c23dea9e02b58759a8998)
+                    URL_HASH SHA256=6a1426381b3966930dc36f30d85a95ccf6855add27b7cf49775b5d65b2902ac4)
 declare_datasource (FILE mini_gen2.genome
                     URL ${CMAKE_SOURCE_DIR}/test/data/mini_gen2.genome
-                    URL_HASH SHA256=7e21b3eff20f950e6a0fa2369a0bbbed4cc967a24083876350f839f4a232771b)
+                    URL_HASH SHA256=6a1426381b3966930dc36f30d85a95ccf6855add27b7cf49775b5d65b2902ac4)
