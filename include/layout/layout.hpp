09e7e8638434ca106210d6d9264a35d0d4d50dd4
Enrico Seiler <enrico.seiler@hotmail.de>
2026-01-27 20:59:17 +0100

    feat: add chopper fast-layout wrapper

---
+++ include/layout/layout.hpp
@@ -0,0 +1,14 @@
+// SPDX-FileCopyrightText: 2006-2025 Knut Reinert & Freie Universität Berlin
+// SPDX-FileCopyrightText: 2016-2025 Knut Reinert & MPI für molekulare Genetik
+// SPDX-License-Identifier: BSD-3-Clause
+
+/*!\file
+ * \brief Provides chopper_layout.
+ * \author Enrico Seiler <enrico.seiler AT fu-berlin.de>
+ */
+
+#pragma once
+
+#include <sharg/parser.hpp>
+
+void chopper_layout(sharg::parser & parser);
